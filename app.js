ed42faa819be9d758451dd49f392226185ce8dcd
Oscar Sanchez <osasanchezme@unal.edu.co>
2022-11-10 08:26:37 -0500

    Creación de todos los inputs y lectura de datos de entrada

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#plot_btn").on("click", function(){
+        var v0 =  $("#v0").val();
+        var alpha =  $("#alpha").val();
+        var k =  $("#k").val();
+        var H =  $("#H").val();
+        var L =  $("#L").val();
+        var d0 =  $("#do").val();
+        var n_esc =  $("#n_esc").val();
+        var n_iter =  $("#n_iter").val();
+    });
+    // alert("I am an alert!");
+    // console.log("I am ready!")
+});
+
+var a = "Dinamica"
